720568b39810984db2f31f237fc3380e5ccaf1aa
VCarames <109115310+VCarames@users.noreply.github.com>
2024-08-04 16:20:29 -0700

    Header: Refactor Layout and Add CartButton and User Image
    Updated layout with separate containers for Navbar, logo, CartButton, and user image. Applied new styles for improved header structure.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,25 @@
 import { Link } from "react-router-dom";
 import Logo from "/images/logo.svg";
 import Navbar from "../navbar/Navbar";
+import CartButton from "../../cart/CartButton";
+import User from "/images/image-avatar.png";
 
 function Header() {
   return (
-    <header>
-      <div>
-        <Navbar />
-        <Link to="/" aria-label="Snearkers - Home">
-          <img src={Logo} alt="" />
-        </Link>
-      </div>
+    <header className="header">
+      <div className="header__content">
+        <div className="header__container header__container--one">
+          <Navbar />
+          <Link className="header__logo" to="/" aria-label="Snearkers - Home">
+            <img src={Logo} alt="" />
+          </Link>
+        </div>
 
-      <p>Cart</p>
-      <p>Avatar</p>
+        <div className="header__container header__container--two">
+          <CartButton />
+          <img className="header__user" src={User} alt="User" />
+        </div>
+      </div>
     </header>
   );
 }
