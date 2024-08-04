e06ea382b9739a1ce53a19c22f19f23e0c192eb4
VCarames <109115310+VCarames@users.noreply.github.com>
2024-08-04 16:17:24 -0700

    NavMenu: Integrate NavButton And Refactor Layout
    Added NavButton component to the NavMenu, included a ref for closing the navigation, and refactored the layout to use a div wrapper instead of a ul.

--- src/components/navbar/NavMenu.jsx
+++ src/components/navbar/NavMenu.jsx
@@ -1,39 +1,51 @@
 import { Link } from "react-router-dom";
+import NavButton from "./NavButton";
+import { useRef } from "react";
 
-const NavMenu = ({ isNavExpanded, navMenuRef, handleFocusOut }) => (
-  <ul
-    className={`nav__menu ${isNavExpanded ? "expanded" : ""}`}
-    id="nav__menu"
-    role="list"
-    ref={navMenuRef}
-    onBlur={handleFocusOut}
-  >
-    <li className="nav__menu-item" role="listitem">
-      <Link className="nav__menu-link" to="/about">
-        Collections
-      </Link>
-    </li>
-    <li className="nav__menu-item" role="listitem">
-      <Link className="nav__menu-link" to="/locations">
-        Men
-      </Link>
-    </li>
-    <li className="nav__menu-item" role="listitem">
-      <Link className="nav__menu-link" to="/contact">
-        Women
-      </Link>
-    </li>
-    <li className="nav__menu-item" role="listitem">
-      <Link className="nav__menu-link" to="/contact">
-        About
-      </Link>
-    </li>
-    <li className="nav__menu-item" role="listitem">
-      <Link className="nav__menu-link" to="/contact">
-        Contact
-      </Link>
-    </li>
-  </ul>
-);
+const NavMenu = ({ isNavExpanded, navMenuRef, handleFocusOut, toggleNav }) => {
+  const closeNavButtonRef = useRef();
+
+  return (
+    <div
+      className={`nav__menu ${isNavExpanded ? "expanded" : ""}`}
+      id="nav__menu"
+      ref={navMenuRef}
+      onBlur={handleFocusOut}
+    >
+      <NavButton
+        isNavExpanded={isNavExpanded}
+        toggleNav={toggleNav}
+        navToggleRef={closeNavButtonRef}
+      />
+      <ul className="nav__menu-list" role="list">
+        <li className="nav__menu-item" role="listitem">
+          <Link className="nav__menu-link" to="/about">
+            Collections
+          </Link>
+        </li>
+        <li className="nav__menu-item" role="listitem">
+          <Link className="nav__menu-link" to="/locations">
+            Men
+          </Link>
+        </li>
+        <li className="nav__menu-item" role="listitem">
+          <Link className="nav__menu-link" to="/contact">
+            Women
+          </Link>
+        </li>
+        <li className="nav__menu-item" role="listitem">
+          <Link className="nav__menu-link" to="/contact">
+            About
+          </Link>
+        </li>
+        <li className="nav__menu-item" role="listitem">
+          <Link className="nav__menu-link" to="/contact">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default NavMenu;
