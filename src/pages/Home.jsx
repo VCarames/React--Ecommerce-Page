2e680613a1fee3406d07136a17be4a49903afcbd
VCarames <109115310+VCarames@users.noreply.github.com>
2024-08-09 22:36:08 -0700

    Home: Display List Of Products
    Updated the “Home” component to fetch and display a list of products using the “useLoaderData” hook. Integrated the “ProductItem” component to show individual product details. The “loader” function fetches product data and provides it to the component for rendering.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
+import { useLoaderData } from "react-router-dom";
+import { getProduct } from "../api/apiProducts";
+import Product from "../product/ProductItem";
+
 function Home() {
+  const product = useLoaderData();
+
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="product">
+      {product.map((product) => (
+        <Product product={product} key={product.id} />
+      ))}
     </div>
   );
+}
+
+export async function loader() {
+  const menu = await getProduct();
+  return menu;
 }
 
 export default Home;
