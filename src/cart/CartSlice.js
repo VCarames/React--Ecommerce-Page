6373f5d2284b5c66030e3f4a86166ca8a8311d59
VCarames <109115310+VCarames@users.noreply.github.com>
2024-08-09 21:53:41 -0700

    CartSlice: Update getCurrentQuantityById Default Value
    Refactored the “getCurrentQuantityById” selector to return a default quantity of 1 if the item is not found in the cart.

--- src/cart/CartSlice.js
+++ src/cart/CartSlice.js
@@ -70,4 +70,4 @@ export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
 
 export const getCurrentQuantityById = (id) => (state) =>
-  state.cart.cart.find((item) => item.productId === id)?.quantity ?? 0;
+  state.cart.cart.find((item) => item.productId === id)?.quantity ?? 1;
