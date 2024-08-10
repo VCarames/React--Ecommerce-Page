6bf0cb6c9175dc80a7336ace318784dd1acd08b3
VCarames <109115310+VCarames@users.noreply.github.com>
2024-08-09 22:14:20 -0700

    UpdateItemQuantity Component Created
    Created a new “UpdateItemQuantity” component to adjust product quantities in the cart using Redux actions. It utilizes “useDispatch” from “react-redux” and includes buttons with icons for increasing or decreasing the quantity.

---
+++ src/product/UpdateItemQuantity.jsx
@@ -0,0 +1,31 @@
+import { useDispatch } from "react-redux";
+import {
+  decreaseItemQuantity,
+  increaseItemQuantity,
+} from "../cart/CartSlice.js";
+
+function UpdateItemQuantity({ productId, currentQuantity }) {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="quantity">
+      <button
+        className=" quantity__button quantity__button--decrease"
+        onClick={() => dispatch(decreaseItemQuantity(productId))}
+        aria-label="Decrease quantity"
+      >
+        <img src="/images/icons/icon-minus.svg" />
+      </button>
+      <span className="quantity__value">{currentQuantity}</span>
+      <button
+        className="quantity__button quantity__button--increase"
+        onClick={() => dispatch(increaseItemQuantity(productId))}
+        aria-label="Increase quantity"
+      >
+        <img src="/images/icons/icon-plus.svg" />
+      </button>
+    </div>
+  );
+}
+
+export default UpdateItemQuantity;
