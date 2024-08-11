7974c50e3d526ea79a3cd2497cca3d5df3b2751b
VCarames <109115310+VCarames@users.noreply.github.com>
2024-08-11 16:46:33 -0700

    UpdateItemQuantity: Refine Styling And Replace Icons
    The “UpdateItemQuantity” component's class names have been updated to “quantity-control” for improved clarity and consistency. The image-based icons were replaced with custom SVG icons to enhance scalability and maintain a consistent design across the application.

--- src/product/UpdateItemQuantity.jsx
+++ src/product/UpdateItemQuantity.jsx
@@ -8,21 +8,47 @@ function UpdateItemQuantity({ productId, currentQuantity }) {
   const dispatch = useDispatch();
 
   return (
-    <div className="quantity">
+    <div className="quantity-control">
       <button
-        className=" quantity__button quantity__button--decrease"
+        className="quantity-control__button quantity-control__button--decrease"
         onClick={() => dispatch(decreaseItemQuantity(productId))}
         aria-label="Decrease quantity"
       >
-        <img src="/images/icons/icon-minus.svg" alt="" />
+        <svg
+          className="quantity-control__button-icon"
+          width="12"
+          height="4"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <path
+              d="M11.357 3.332A.641.641 0 0 0 12 2.69V.643A.641.641 0 0 0 11.357 0H.643A.641.641 0 0 0 0 .643v2.046c0 .357.287.643.643.643h10.714Z"
+              id="a"
+            />
+          </defs>
+          <use xlinkHref="#a" />
+        </svg>
       </button>
-      <span className="quantity__value">{currentQuantity}</span>
+      <span className="quantity-control__value">{currentQuantity}</span>
       <button
-        className="quantity__button quantity__button--increase"
+        className="quantity-control__button quantity-control__button--increase"
         onClick={() => dispatch(increaseItemQuantity(productId))}
         aria-label="Increase quantity"
       >
-        <img src="/images/icons/icon-plus.svg" alt="" />
+        <svg
+          className="quantity-control__button-icon"
+          width="12"
+          height="12"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <path
+              d="M12 7.023V4.977a.641.641 0 0 0-.643-.643h-3.69V.643A.641.641 0 0 0 7.022 0H4.977a.641.641 0 0 0-.643.643v3.69H.643A.641.641 0 0 0 0 4.978v2.046c0 .356.287.643.643.643h3.69v3.691c0 .356.288.643.644.643h2.046a.641.641 0 0 0 .643-.643v-3.69h3.691A.641.641 0 0 0 12 7.022Z"
+              id="b"
+            />
+          </defs>
+          <use xlinkHref="#b" />
+        </svg>
       </button>
     </div>
   );
