30b70fe7998780a1cefc879666593313e097c314
VCarames <109115310+VCarames@users.noreply.github.com>
2024-08-11 17:12:25 -0700

    Header: Class Renaming And Cart Integration
    Class names have been updated to “header__group” for improved structure. “CartButton” was replaced with the “Cart”component to provide direct access to the cart within the header.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,23 @@
 import { Link } from "react-router-dom";
 import Logo from "/images/logo.svg";
 import Navbar from "../navbar/Navbar";
-import CartButton from "../../cart/CartButton";
 import User from "/images/image-avatar.png";
+import Cart from "../../cart/Cart";
 
 function Header() {
   return (
     <header className="header">
       <div className="header__content">
-        <div className="header__container header__container--one">
+        <div className="header__group header__group--primary">
           <Navbar />
-          <Link className="header__logo" to="/" aria-label="Snearkers - Home">
+          <Link className="header__logo" to="/" aria-label="Sneakers - Home">
             <img src={Logo} alt="" />
           </Link>
         </div>
 
-        <div className="header__container header__container--two">
-          <CartButton />
-          <img className="header__user" src={User} alt="User" />
+        <div className="header__group header__group--secondary">
+          <Cart />
+          <img className="header__user-avatar" src={User} alt="User" />
         </div>
       </div>
     </header>
